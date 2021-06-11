0f1a1d4dc832e633718252f224527f268a24192d
Pierrick <mulotmail@gmail.com>
2021-06-11 15:29:25 +0200

    Icons

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
     <b-container>
-      <b-row class="mt-5 mb-5">
+      <b-row class="mt-5 mb-4">
         <b-col>
-          <h1>gbfs-validator.netlify.com</h1>
-          <h2>A GBFS feed validator</h2>
-          <p>Validate any <a href="https://github.com/NABSA/gbfs/">GBFS</a> feed online. Based on official <a href="https://github.com/MobilityData/gbfs-json-schema">GBFS json schemas</a>. <br>
+          <b-row class="mb-2">
+            <b-col class="flex-grow-0">
+              <img src="/gbfs_validator.jpg" alt="">
+            </b-col>
+            <b-col>
+              <h1>gbfs-validator.netlify.com</h1>
+              <h2>A GBFS feed validator</h2>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <p>Validate any <a href="https://github.com/NABSA/gbfs/">GBFS</a> feed online. Based on official <a href="https://github.com/MobilityData/gbfs-json-schema">GBFS json schemas</a>. <br>
           More details on <a href="https://github.com/fluctuo/gbfs-validator">https://github.com/fluctuo/gbfs-validator</a>.</p>
+            </b-col>
+          </b-row>
         </b-col>
       </b-row>
       <b-row>
