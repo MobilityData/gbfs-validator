a77e9ad8d44cabcb397896eadfea986aaf30bb3e
Pierrick <mulotmail@gmail.com>
2022-02-09 17:03:01 +0100

    Optimise bootstrap build

--- website/src/main.js
+++ website/src/main.js
@@ -2,14 +2,38 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import { BootstrapVue } from 'bootstrap-vue'
+import {
+  AlertPlugin,
+  ButtonPlugin,
+  CardPlugin,
+  CollapsePlugin,
+  FormCheckboxPlugin,
+  FormGroupPlugin,
+  FormInputPlugin,
+  FormPlugin,
+  FormSelectPlugin,
+  LayoutPlugin,
+  TabsPlugin,
+  TooltipPlugin
+} from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
-Vue.use(BootstrapVue)
+Vue.use(AlertPlugin)
+Vue.use(ButtonPlugin)
+Vue.use(CardPlugin)
+Vue.use(CollapsePlugin)
+Vue.use(FormCheckboxPlugin)
+Vue.use(FormGroupPlugin)
+Vue.use(FormInputPlugin)
+Vue.use(FormPlugin)
+Vue.use(FormSelectPlugin)
+Vue.use(LayoutPlugin)
+Vue.use(TabsPlugin)
+Vue.use(TooltipPlugin)
 
 Vue.filter('formatNumber', function(value) {
   return new Intl.NumberFormat().format(value)
