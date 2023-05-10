c855360f8c53e0f027a4d3f771e62e88d1183064
David Gamez Diaz <1192523+davidgamez@users.noreply.github.com>
2023-05-10 16:28:40 -0400

    add validator summary function

---
+++ functions/validator-summary.js
@@ -0,0 +1,39 @@
+const GBFS = require('gbfs-validator')
+
+const getSummary = (validationResult) => (
+  {
+    ...validationResult,
+    files: undefined,
+    filesIncluded: Object.keys((validationResult.files || {}))
+  }
+)
+
+exports.handler = function(event, context, callback) {
+  let body
+
+  try {
+    body = JSON.parse(event.body)
+  } catch (err) {
+    callback(err, {
+      statusCode: 500,
+      body: JSON.stringify(err)
+    })
+  }
+
+  const gbfs = new GBFS(body.url, body.options)
+
+  gbfs
+    .validation()
+    .then(result => {
+      callback(null, {
+        statusCode: 200,
+        body: getSummary(result)
+      })
+    })
+    .catch(err => {
+      callback(null, {
+        statusCode: 500,
+        body: JSON.stringify(err.message)
+      })
+    })
+}
