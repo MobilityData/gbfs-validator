5f6ab4dc02de71a691ebeda364a70736e65dbe20
David Gamez Diaz <1192523+davidgamez@users.noreply.github.com>
2023-06-06 10:30:36 -0400

    add function documentation

--- functions/validator-summary.js
+++ functions/validator-summary.js
@@ -1,5 +1,33 @@
 const GBFS = require('gbfs-validator')
 
+/** @typedef {{
+ *             summary: {
+ *               validatorVersion: string,
+ *               hasErrors: boolean,
+ *               errorsCount: number,
+ *               version: {
+ *                 detected: string,
+ *                 validated: string  
+ *                }
+ *               filesSummary: [
+ *                {
+ *                  required: boolean,
+ *                  exists: boolean,
+ *                  hasErrors: boolean,
+ *                  file: string,
+ *                  errorsCount: number
+ *                }
+ *               ]
+ *              }
+ *           }} Summary
+ */
+
+/**
+ * This function returns a summary from the validator's response stripping out the notices.
+ * 
+ * @param validationResult from the GBFS validator class
+ * @returns { Summary }
+ */
 const getSummary = (validationResult) => (
   {
     ...validationResult,
@@ -14,32 +42,35 @@ const getSummary = (validationResult) => (
   }
 )
 
-exports.handler = function (event, context, callback) {
-  let body
+  /**
+   * call the callback function with {@link Summary}
+   */
+  exports.handler = function (event, context, callback) {
+    let body
 
-  try {
-    body = JSON.parse(event.body)
-  } catch (err) {
-    callback(err, {
-      statusCode: 500,
-      body: JSON.stringify(err)
-    })
-  }
-
-  const gbfs = new GBFS(body.url, body.options)
-
-  gbfs
-    .validation()
-    .then(result => {
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(getSummary(result))
-      })
-    })
-    .catch(err => {
-      callback(null, {
+    try {
+      body = JSON.parse(event.body)
+    } catch (err) {
+      callback(err, {
         statusCode: 500,
-        body: JSON.stringify(err.message)
+        body: JSON.stringify(err)
       })
-    })
-}
+    }
+
+    const gbfs = new GBFS(body.url, body.options)
+
+    gbfs
+      .validation()
+      .then(result => {
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(getSummary(result))
+        })
+      })
+      .catch(err => {
+        callback(null, {
+          statusCode: 500,
+          body: JSON.stringify(err.message)
+        })
+      })
+  }
