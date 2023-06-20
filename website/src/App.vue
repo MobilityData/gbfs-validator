be8c07780c0a0573641577fdddf7da1ca7324abd
Josee Sabourin <66266820+josee-sabourin@users.noreply.github.com>
2023-06-20 10:30:10 -0400

    Update App.vue to proper URLs
    Change gbfs-validator.netlify.com -> gbfs-validator.mobilitydata.org
    Change github.com/NABSA/gbfs -> github.com/MobilityData/gbfs

--- website/src/App.vue
+++ website/src/App.vue
@@ -13,7 +13,7 @@ import Footer from './components/Footer.vue'
               <img src="/gbfs_validator.jpg" alt="" />
             </b-col>
             <b-col>
-              <h1>gbfs-validator.netlify.com</h1>
+              <h1>gbfs-validator.mobilitydata.org</h1>
               <h2>A GBFS feed validator</h2>
             </b-col>
           </b-row>
@@ -21,7 +21,7 @@ import Footer from './components/Footer.vue'
             <b-col>
               <p>
                 Validate any
-                <a href="https://github.com/NABSA/gbfs/">GBFS</a> feed online.
+                <a href="https://github.com/MobilityData/gbfs/">GBFS</a> feed online.
                 Based on official
                 <a href="https://github.com/MobilityData/gbfs-json-schema"
                   >GBFS json schemas</a
