711e3b879ee2ba9fc606a772221278c09cf464f2
Tom <1955774+tdelmas@users.noreply.github.com>
2023-06-28 10:31:10 +0200

    update test for 3.0-RC , 3.0 and upcoming minor versions
    Signed-off-by: Tom <1955774+tdelmas@users.noreply.github.com>

--- gbfs-validator/gbfs.js
+++ gbfs-validator/gbfs.js
@@ -311,7 +311,8 @@ class GBFS {
 
       let version = this.options.version || this.autoDiscovery.version
 
-      if (version === '3.0-RC') {
+      // 3.0-RC , 3.0 and upcoming minor versions
+      if (/^3\.\d/.test(version)) {
         urls = this.autoDiscovery.data.feeds?.filter(f => f.name === type) || []
       } else {
         urls = Object.entries(this.autoDiscovery.data).map(key => {
